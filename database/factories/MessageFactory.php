994428f97a432b4862279222d74d8ccc7657ff36
samuel-bie <samuel.bie75@gmail.com>
2023-10-13 21:26:14 +0200

    Refactor faker db

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -19,7 +19,7 @@ class MessageFactory extends Factory
     {
         return [
             'content' => $this->faker->paragraph,
-            'image' => $this->faker->imageUrl(),
+            'image' => $this->faker->imageUrl(), // Example for generating image URLs
             'sender_user_id' => User::inRandomOrder()->first()->id,
         ];
     }
